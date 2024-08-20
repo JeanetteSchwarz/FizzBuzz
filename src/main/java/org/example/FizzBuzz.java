102dc048bb189f044d3f778ebabd83643607b576
JeanetteSchwarz <jeanette.schwarz@t-online.de>
2024-08-20 12:35:56 +0200

    Final

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -2,7 +2,22 @@ package org.example;
 
 public class FizzBuzz {
 
+    /**
+     * FizzBuzz Anforderungen:
+     * 1. Zahl durch 3 teilbar? = Fizz V
+     * 2. Zahl durch 5 teilbar? = Buzz V
+     * 3. Zahl durch 3&5 teilbar? = FizzBuzz V
+     * 4. Sonst Zahl als String! V
+     */
+
     public static String play(int number) {
-        return "" +number;
+        if (number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        }
+        return ""+number;
     }
 }
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
 @Test
-    void play_shouldReturn1_whenCalledWith1() {
+void play_shouldReturn1_whenCalledWith1(){
     //GIVEN
     int number = 1;
     String expected = "1";
@@ -15,4 +15,103 @@ class FizzBuzzTest {
     //THEN
     assertEquals(expected, actual);
 }
+
+    @Test
+    void play_shouldReturn4_whenCalledWith4(){
+        //GIVEN
+        int number = 4;
+        String expected = "4";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizz_whenCalledWith3(){
+        //GIVEN
+        int number = 3;
+        String expected = "Fizz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizz_whenCalledWith6(){
+        //GIVEN
+        int number = 6;
+        String expected = "Fizz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnBuzz_whenCalledWith5(){
+        //GIVEN
+        int number = 5;
+        String expected = "Buzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnBuzz_whenCalledWith10(){
+        //GIVEN
+        int number = 10;
+        String expected = "Buzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWith15(){
+        //GIVEN
+        int number = 15;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWith0(){
+        //GIVEN
+        int number = 0;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWithMinus1(){
+        //GIVEN
+        int number = -1;
+        String expected = "-1";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void play_shouldReturnFizzBuzz_whenCalledWithMinus15(){
+        //GIVEN
+        int number = -15;
+        String expected = "FizzBuzz";
+        //WHEN
+        String actual = FizzBuzz.play(number);
+        //THEN
+        assertEquals(expected, actual);
+    }
 }
